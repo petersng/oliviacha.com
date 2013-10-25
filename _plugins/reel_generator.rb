b543c4f3c438aa8b7f8c125377df06a74aad4347
petersng <peter.s.ng@gmail.com>
2013-10-25 13:47:41 -0400

    Now jekyll generator can directly pull videos from vimeo based on links file.  awesome.

--- _plugins/reel_generator.rb
+++ _plugins/reel_generator.rb
@@ -1,4 +1,9 @@
+require "vimeo"
+require "uri"
+require "json"
+
 module Jekyll
+
     class ReelsPage < Page
         def initialize(site, base, dir, name, sections)
             super(site, base, dir, name)
@@ -6,39 +11,73 @@ module Jekyll
             self.data['sections'] = sections
         end
     end
+
+
     class ReelsGenerator < Generator
         def generate(site)
-            sections = [{
-                'name' => 'tv',
-                'links' => [
-                    {
-                        'id' => 76516796,
-                        'position' => 1,
-                        'thumbnail' => 'http://b.vimeocdn.com/ts/451/349/451349356_200.jpg'
-                    },
-                    {
-                        'id' => 77002689,
-                        'position' => 2,
-                        'thumbnail' => 'http://b.vimeocdn.com/ts/452/046/452046229_200.jpg'
-                    },
-                    {
-                        'id' => 77002689,
-                        'position' => 3,
-                        'thumbnail' => 'http://b.vimeocdn.com/ts/452/046/452046229_200.jpg'
-                    },
-                    {
-                        'id' => 77002689,
-                        'position' => 4,
-                        'thumbnail' => 'http://b.vimeocdn.com/ts/452/046/452046229_200.jpg'
-                    },
-                    {
-                        'id' => 77002689,
-                        'position' => 5,
-                        'thumbnail' => 'http://b.vimeocdn.com/ts/452/046/452046229_200.jpg'
-                    }
-                ]
-            }]
+            vimeo = get_vimeo_session
+            sections = read_links_section_file(vimeo)
             site.pages << ReelsPage.new(site, site.source, 'reels', 'index.html', sections)
+        end
+        def get_vimeo_session()
+            Vimeo::Advanced::Video.new("7598b66411cf608028fa81754640d69a17f23bb2",
+                "f050e42e8d1df675c99a704f3222452639ec4a0c",
+                :token => '6c617d440d5dfea4d27e9c3a9ed6dfee',
+                :secret => '98b0da9d928e35c4467a25959250dee8f7934850')
+        end
+        def strip_id_from_vimeo_link(link)
+            parsed = URI(link)
+            path = parsed.path
+            path.split("/")[1].strip
+        end
+        def get_vimeo_video_info(vimeo, video_id)
+            data = vimeo.get_info(video_id)
+            data['video'][0]
+        end
+        def get_vimeo_video_thumbnail(vimeo, video_id, width, height)
+            video_data = get_vimeo_video_info(vimeo, video_id)
+            thumbnails = video_data['thumbnails']['thumbnail']
+            thumbnails.each do |thumb|
+                if width > 0 and height > 0
+                    if thumb['width'].to_i == width and thumb['height'].to_i == height
+                        return thumb['_content']
+                    end
+                end
+            end
+            return ''
+        end
+        def read_links_section_file(vimeo)
+            sections_links = []
+            section = nil
+            position = 0
+            File.open('vimeo_links.txt').each_line do |line|
+                line.rstrip!
+                if line.start_with?('#') or line == ''
+                    next
+                elsif line.start_with?('>>')
+                    if section != nil
+                        sections_links.push(section)
+                    end
+                    section_name = line.sub('>>', '')
+                    section = {
+                        'name' => section_name,
+                        'links' => []
+                    }
+                    position = 1
+                else
+                    link = line
+                    video_id = strip_id_from_vimeo_link(link)
+                    section['links'].push({
+                        'url' => link,
+                        'id' => video_id,
+                        'position' => position,
+                        'thumbnail' => get_vimeo_video_thumbnail(
+                            vimeo, video_id, 200, 150)
+                    })
+                    position = position + 1
+                end
+            end
+            sections_links
         end
     end
 end
